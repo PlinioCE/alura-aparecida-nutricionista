5bfb14618692e70876d5ad7525fa4fadf2b692e8
Plinio Albuquerque <pliniomalbuquerque@gmail.com>
2023-05-18 18:40:52 -0300

    Adicionada função de legenda colorida

--- js/busca-paciente.js
+++ js/busca-paciente.js
@@ -11,6 +11,7 @@ botaoBuscar.addEventListener("click", function () {
             adicionarPacienteNaTabela(paciente);
         });
         pacientes = document.querySelectorAll(".paciente");
+        // adicionaLegendaColorida();
     });
 
     xhr.send();
